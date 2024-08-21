24d4078e6c11873d5a5fe732242e8652959bc5fa
Jaxon Adams <jadamsresume1@gmail.com>
2024-08-21 16:24:15 -0600

    booster particles and some refactoring

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,9 @@ public class Movement : MonoBehaviour
     [SerializeField] float rotationThrust = 100f;
     [SerializeField] float mainThrust = 800f;
     [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem mainBoosterParticles;
+    [SerializeField] ParticleSystem leftBoosterParticles;
+    [SerializeField] ParticleSystem rightBoosterParticles;
 
     Rigidbody rb;
     AudioSource audioSource;
@@ -26,12 +29,11 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            if (!audioSource.isPlaying) audioSource.PlayOneShot(mainEngine);
-            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            StartThrusting();
         }
         else
         {
-            audioSource.Stop();
+            StopThrusting();
         }
     }
 
@@ -39,11 +41,15 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            ApplyRotation(rotationThrust);
+            StartLeftRotation();
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            ApplyRotation(-rotationThrust);
+            StartRightRotation();
+        }
+        else
+        {
+            StopRotation();
         }
     }
 
@@ -52,6 +58,37 @@ public class Movement : MonoBehaviour
         rb.freezeRotation = true;  // Freezing rotation so we can manually rotate
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rb.freezeRotation = false; // Allowing physics system to take over again
+    }
+
+    void StartThrusting()
+    {
+        if (!audioSource.isPlaying) audioSource.PlayOneShot(mainEngine);
+        if (!mainBoosterParticles.isPlaying) mainBoosterParticles.Play();
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+    }
+
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainBoosterParticles.Stop();
+    }
+
+    void StartLeftRotation()
+    {
+        if (!rightBoosterParticles.isPlaying) rightBoosterParticles.Play();
+        ApplyRotation(rotationThrust);
+    }
+
+    void StartRightRotation()
+    {
+        if (!leftBoosterParticles.isPlaying) leftBoosterParticles.Play();
+        ApplyRotation(-rotationThrust);
+    }
+
+    void StopRotation()
+    {
+        rightBoosterParticles.Stop();
+        leftBoosterParticles.Stop();
     }
 
 }
